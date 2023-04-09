99a641f11502279fab1817beb6ebe08de4e1b2ef
aaronzyf <aaron.zyf@qq.com>
2023-04-09 13:29:25 +0800

    Optimize test data

--- src/com/cabbage03/studentManagement/Main.java
+++ src/com/cabbage03/studentManagement/Main.java
@@ -12,9 +12,15 @@ public class Main {
     public static void main(String[] args) {
 
         List<Student> list = new ArrayList<>();
-        for (int i = 0; i < 5; i++) {
-            Student wuqiongStudent = new Student("吴琼" + i, Gender.WOMAN, 1);
-            list.add(wuqiongStudent);
+        for (int i = 0; i < 15; i++) {
+            if(i%2 == 0){
+                Student wuqiongStudent = new Student("吴琼" + i, Gender.WOMAN, i%4+1);
+                list.add(wuqiongStudent);
+            }else{
+                Student wuqiongStudent = new Student("小张同学" + i, Gender.MAN, i%4+1);
+                list.add(wuqiongStudent);
+            }
+
         }
 
         CommandLineUtils.tableData(list);
--- src/com/cabbage03/studentManagement/tools/CommandLineUtils.java
+++ src/com/cabbage03/studentManagement/tools/CommandLineUtils.java
@@ -1,4 +1,4 @@
-package  com.cabbage03.studentManagement.tools;
+package com.cabbage03.studentManagement.tools;
 
 import com.cabbage03.studentManagement.annotations.TableColumnWidth;
 import com.cabbage03.studentManagement.annotations.TableDataField;
@@ -13,76 +13,84 @@ import java.util.concurrent.atomic.AtomicInteger;
 public class CommandLineUtils {
 
 
-    public static boolean deleteConfirm(String deleteTip){
+    public static boolean deleteConfirm(String deleteTip) {
 
-        System.out.println(  deleteTip+ " (y/n)");
+        System.out.println(deleteTip + " (y/n)");
         Scanner scanner = new Scanner(System.in);
 
         //获取用户的输入 , 并且将用户的输入转成小写的形式.
         String input = scanner.nextLine().toLowerCase();
 
-        if(input.equals("y")){
+        if (input.equals("y")) {
             return true;
-        }else if(input.equals("n")){
+        } else if (input.equals("n")) {
             return false;
-        }else{
+        } else {
             return false;
         }
     }
 
-    public static boolean deleteConfirm(){
+    public static boolean deleteConfirm() {
         return deleteConfirm("确定要删除吗");
     }
 
+    private static final int DEFAULT_COLUMN_WIDTH = 10;
+
     /**
      * 只显示实体类中 , 被 TableDataField 装饰的类.
+     *
      * @param list
      * @param cls
      */
-    public static void tableData(List<?> list,Class<?> cls){
+    public static void tableData(List<?> list, Class<?> cls) {
+
         Field[] fields = cls.getDeclaredFields();
         List<Field> fieldsShowInTable = new ArrayList<>();
-        System.out.print( "#" + "\t");
+        System.out.print("#" + "\t");
 
-        for (Field field:fields) {
+        for (Field field : fields) {
             TableDataField tableDataField = field.getAnnotation(TableDataField.class);
             TableColumnWidth tableColumnWidth = field.getAnnotation(TableColumnWidth.class);
-            if(tableDataField != null){
-                System.out.print( StringUtils.padString(tableDataField.value(),tableColumnWidth == null ? 20 : tableColumnWidth.value() , StringUtils.StringAlignment.RIGHT) + "\t\t");
+            if (tableDataField != null) {
+                System.out.print(StringUtils.padString(tableDataField.value(), tableColumnWidth == null ? DEFAULT_COLUMN_WIDTH : tableColumnWidth.value(), StringUtils.StringAlignment.RIGHT) + "\t\t");
                 fieldsShowInTable.add(field);
             }
         }
 
 
         System.out.println();
-        if(list.isEmpty()){
+        if (list.isEmpty()) {
             System.out.println();
             System.out.println("\t\t\tno data");
         }
 
         AtomicInteger index = new AtomicInteger();
-        list.forEach((Object obj)->{
-            System.out.print(index.incrementAndGet()+ "\t");
-            fieldsShowInTable.forEach((Field field)->{
+        list.forEach((Object obj) -> {
+            System.out.print(index.incrementAndGet() + "\t");
+            fieldsShowInTable.forEach((Field field) -> {
                 //判断该属性有没有get方法 , 如果有 则invoke 对应的get方法
                 try {
                     TableColumnWidth tableColumnWidth = field.getAnnotation(TableColumnWidth.class);
                     System.out.printf(
-                            StringUtils.padString(cls.getMethod("get"+StringUtils.capitalize(field.getName())).invoke(obj).toString(),tableColumnWidth == null ? 20 : tableColumnWidth.value(), StringUtils.StringAlignment.RIGHT)
-                                    +"\t\t");
+                            StringUtils.padString(
+                                    cls.getMethod("get" + StringUtils.capitalize(field.getName())).invoke(obj).toString(),
+                                    tableColumnWidth == null ? DEFAULT_COLUMN_WIDTH : tableColumnWidth.value(),
+                                    StringUtils.StringAlignment.RIGHT)
+                                    + "\t\t");
                 } catch (NoSuchMethodException | InvocationTargetException | IllegalAccessException e) {
-                    System.out.print("no data"+"\t\t");
+                    System.out.print("no data" + "\t\t");
                 }
             });
             System.out.println();
         });
     }
-    public static void tableData(List<?> list){
+
+    public static void tableData(List<?> list) {
         if (list.isEmpty()) {
             System.out.println("No data available.");
             return;
         }
         Class<?> cls = list.get(0).getClass();
-        tableData(list,cls);
+        tableData(list, cls);
     }
 }
